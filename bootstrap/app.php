f418a558d18b840f59640e22f515f379543d609e
bayprmdy19 <bayusmp71@gmail.com>
2026-07-20 22:12:44 +0700

    route untuk foto berita

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +16,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Konfigurasi public path kustom secara dinamis untuk shared hosting / produksi
+$publicPathEnv = env('PUBLIC_PATH');
+if ($publicPathEnv) {
+    if (str_starts_with($publicPathEnv, '..')) {
+        $app->usePublicPath(realpath(base_path($publicPathEnv)) ?: base_path($publicPathEnv));
+    } else {
+        $app->usePublicPath($publicPathEnv);
+    }
+} elseif (basename(base_path()) === 'laravel-core') {
+    $app->usePublicPath(dirname(base_path()));
+}
+
+return $app;
